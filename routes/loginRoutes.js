6070265b5b0ba2f45e8e2ff034d0b2dbe32fb1c4
sagarvanave <ssv1437@gmail.com>
2022-05-20 13:07:27 +0530

    Unwanted code is removed from login and register route

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const bodyParser = require("body-parser");
 const User = require('../schemas/UserSchema');
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
 
 router.get("/", (req, res, next) => {
     res.status(200).render("login");
