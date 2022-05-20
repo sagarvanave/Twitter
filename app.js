4fb526cc3ff5072c5aecdf7440b9b7fc948c9f82
sagarvanave <ssv1437@gmail.com>
2022-05-20 09:52:56 +0530

    Registration page database connection is done.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser=require('body-parser');
 
 //App engine set to hbs because we are using handlebars 
 app.set('view engine', 'hbs');
@@ -17,6 +18,8 @@ mongoose.connect("mongodb://localhost:27017/twitter")
 
 //Starting Server
 const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/",(req, res) => {
     res.send("Hello there, <br> This is simple twitter app");
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const User = require('../schemas/UserSchema');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
     res.status(200).render("register");
+});
+
+router.post("/", async (req, res, next) => {
+    
+    const firstName = req.body.firstName.trim();
+    const lastName = req.body.lastName.trim();
+    const username = req.body.userName.trim();
+    const email = req.body.email.trim();
+    const password = req.body.password;
+
+    var data=req.body;
+    data.password=await bcrypt.hash(password,10);
+
+    console.log(data);
+
+    User.create(data).then((user) => {
+        console.log("User is registered.");
+    });
+
 });
 
 module.exports = router;
