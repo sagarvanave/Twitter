602be46313e61892373fe34432f62a5baf7fafc5
sagarvanave <ssv1437@gmail.com>
2022-05-20 10:25:57 +0530

    Validation is added for new user registration

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -15,18 +15,50 @@ router.post("/", async (req, res, next) => {
     
     const firstName = req.body.firstName.trim();
     const lastName = req.body.lastName.trim();
-    const username = req.body.userName.trim();
+    const userName = req.body.userName.trim();
     const email = req.body.email.trim();
     const password = req.body.password;
 
-    var data=req.body;
-    data.password=await bcrypt.hash(password,10);
+    var payload=req.body;
 
-    console.log(data);
+    if(firstName && lastName && userName && email && password){
+        //Check if user is already registered
+        const user = await User.findOne({
+            $or:[
+                {userName: userName},
+                {email:email}
+            ]
+        }).catch((error)=>{
+            console.log(error);
+            payload.errorMsg="Something went wrong";
+            res.status(200).render("register",payload);
+        });
 
-    User.create(data).then((user) => {
-        console.log("User is registered.");
-    });
+        if(user == null){
+            //No user found
+            var data=req.body;
+
+            //Password encrypted using bcrypt
+            data.password=await bcrypt.hash(password,10);
+            console.log(data);
+        
+            User.create(data).then((user) => {
+                console.log("User is registered.");
+                return res.redirect('/register');
+            });
+        }else{
+            if(email===user.email){
+                payload.errorMsg="Email already present";
+            }
+            else{
+                payload.errorMsg="Username is already present";
+            }
+            res.status(200).render("register",payload);
+        }   
+    }else{
+        payload.errorMsg="Make sure that each field has a valid value";
+        res.status(200).render("register",payload);
+    }
 
 });
 
