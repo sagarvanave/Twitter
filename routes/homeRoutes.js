dc14c7966891852d758a97a44b7bab40e5bd1dd4
sagarvanave <ssv1437@gmail.com>
2022-05-20 17:09:33 +0530

    Logout functionality is added

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser");
 const User = require('../schemas/UserSchema');
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
     console.log(req.session.user);
-    res.status(200).render("home");
+    res.status(200).render("home",req.session.user);
 });
 
 router.post("/", async (req,res, next) => {
