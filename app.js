5c7d810fd80453b18b077ffe3a6a38146ebd3a56
sagarvanave <ssv1437@gmail.com>
2022-05-20 09:28:08 +0530

    Register page route and UI is created

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//App engine set to hbs because we are using handlebars 
+app.set('view engine', 'hbs');
+app.set("views","views");
+
 //Starting Server
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.get("/",(req, res) => {
     res.send("Hello there, <br> This is simple twitter app");
 });
+
+//Routes
+const registerRoute= require('./routes/registerRoutes');
+
+app.use("/register", registerRoute);
+
+
