204a54a8c7405e76b38ee04e6214d170a0297d35
sagarvanave <ssv1437@gmail.com>
2022-05-20 09:33:28 +0530

    Database connection is done

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 //App engine set to hbs because we are using handlebars 
 app.set('view engine', 'hbs');
 app.set("views","views");
+
+//Database connection setup
+mongoose.connect("mongodb://localhost:27017/twitter")
+.then(() => {
+                console.log("database connection successful");
+}).catch((err) => {
+                console.log("database connection error " + err);
+});
 
 //Starting Server
 const server = app.listen(port, () => console.log("Server listening on port " + port));
